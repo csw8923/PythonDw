f2d8f3d8a25d757a00616294a2903f46736c6554
CSW8923 <CSW8923@126.COM>
2013-02-04 17:35:02 +0800

    下载多线程技术 和 多线程

--- threads1.py
+++ threads1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import threading,time
 class MyThread(threading.Thread):
     def __init__(self,threadname):
@@ -7,6 +8,9 @@ class MyThread(threading.Thread):
         for i in xrange(10):
             print self.getName(),i
             time.sleep(1)
-
-my = MyThread('test')
-my.start()
+#多线程 进程1
+my1 = MyThread('test')
+my1.start()
+#多线程 进程2
+my2 = MyThread('test2')
+my2.start()
