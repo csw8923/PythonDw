8860786d2936621bb0898090749d87503ef2d72d
csw8956 <CSW8923@QQ.COM>
2013-02-05 17:17:37 +0800

    数据连接 例子

---
+++ database.py
@@ -0,0 +1,13 @@
+import MySQLdb
+db = MySQLdb.connect(user='root', db='test', passwd='19890823', host='localhost')
+cursor = db.cursor()
+cursor.execute('SELECT username FROM user ORDER BY id')
+cursor.execute('insert into user values 'sss')
+names = [row[0] for row in cursor.fetchall()]
+#print names
+#print len(names)
+#for name in names:
+#    print name
+#else:
+#    print 'this is over'
+db.close()
