9e49aa7e574eb5dcd324b458958cc6843511435a
csw8956 <CSW8923@QQ.COM>
2013-02-20 08:52:13 +0800

    更新整理文件结构

--- dir.py
+++
@@ -1,3 +0,0 @@
-print "<h1>Squares</h1>" 
-for i in range(10):
-    print "%s :<b>%s</b>" %(i,i*i)
--- readxml.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import android
-from xml.dom import minidom
-b = minidom.parse('ip.xml')
-for i in b.childNodes[0].getElementsByTagName('p'):
-   print i.childNodes[0].toxml()
-for i in b.childNodes[0].getElementsByTagName('t'):
-   print i.childNodes[0].toxml()
